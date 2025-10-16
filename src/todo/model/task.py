247c414be28f8b3a1c76aa10cc1a533664655e96
raha1382 <143132177+raha1382@users.noreply.github.com>
2025-10-16 21:11:51 +0330

    feat: create task class

---
+++ src/todo/model/task.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass, field
+from typing import List
+from datetime import datetime
+
+TASK_STATUS = {"todo" , "doing" , "done"}
+
+class Task:
+    name: int
+    description: str = ""
+    status: str = "todo"
+    deadline : datetime = field(default_factory = datetime.now)
+
+    def __post_init__(self):
+        """Validate initial data after initialization."""
+        if len(self.name.split()) > 30:
+            raise ValueError("Project name exceeds 30 words")
+        if len(self.description.split()) > 150:
+            raise ValueError("Project description exceeds 150 words")
