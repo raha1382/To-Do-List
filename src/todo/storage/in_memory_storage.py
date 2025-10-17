4e2960cab1b465b5aa07718d0569dced62c9eb33
raha1382 <143132177+raha1382@users.noreply.github.com>
2025-10-17 14:02:40 +0330

    feat: create get-project function to get projects from storage by name of project

---
+++ src/todo/storage/in_memory_storage.py
@@ -0,0 +1,15 @@
+from ..model.project import Project
+from ..model.task import Task
+from ..utils.utils import MAX_NUMBER_OF_PROJECTS, MAX_NUMBER_OF_TASKS
+
+class In_Memory_Storage:
+    def __init__(self):
+        self._projects: dict[str, Project] = {}
+        self._tasks: dict[str, Task] = {}
+
+    def add_project(self, project: Project):
+        self._projects[project.name] = project
+
+    def get_project(self, name: str):
+            project = self._projects[name]
+            return project
