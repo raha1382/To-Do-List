ea420ced5287b3291a6b299b792435e7b943c124
raha1382 <143132177+raha1382@users.noreply.github.com>
2025-11-13 23:50:51 +0330

    feat: create table and create connection with oop with usage of sqlalchemy.orm for project and task classes.

--- src/todo/model/task.py
+++ src/todo/model/task.py
@@ -2,15 +2,30 @@ from dataclasses import dataclass, field
 from typing import Optional
 from datetime import datetime
 from ..utils.validators import validate_name_of_task, validate_description_of_task, validate_status_of_task, validate_deadline
+from ..db.base import Base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+import enum
 
 @dataclass
-class Task:
-    id: int
-    title: str
-    description: str = ""
-    status: str = "todo"
-    deadline: Optional[datetime] = field(default_factory=datetime.now)
+class TaskStatus(enum.Enum):
+    TODO = "todo"
+    IN_PROGRESS = "in_progress"
+    DONE = "done"
+
+@dataclass
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title= Column(String, unique=True, nullable=False)
+    description= Column(String, nullable=True)
+    status = Column(enum(TaskStatus), default=TaskStatus.TODO, nullable=False)
+    deadline = Column(DateTime, nullable=True, default=datetime.now)
     project_name: Optional[str] = None
+    project_name = Column(String, ForeignKey("projects.name", ondelete="CASCADE"))
+    project_id = Column(Integer, ForeignKey("projects.id", ondelete="CASCADE"))
+    project = relationship("Project", back_populates="tasks")
 
     def __post_init__(self):
         self.title = validate_name_of_task(self.title)
